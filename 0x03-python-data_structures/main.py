7027d14db14f772b714f29f3d181d0b8268b1dbb
ehisgodswill <ehisgodswill@gmail.com>
2023-12-02 04:46:11 +0100

    14

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python3
-delete_at = __import__('11-delete_at').delete_at
+import ctypes
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+lib = ctypes.CDLL('./libPyList.so')
+lib.print_python_list_info.argtypes = [ctypes.py_object]
+l = ['hello', 'World']
+lib.print_python_list_info(l)
+del l[1]
+lib.print_python_list_info(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+lib.print_python_list_info(l)
+l = []
+lib.print_python_list_info(l)
+l.append(0)
+lib.print_python_list_info(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(l)
+l.pop()
+lib.print_python_list_info(l)
