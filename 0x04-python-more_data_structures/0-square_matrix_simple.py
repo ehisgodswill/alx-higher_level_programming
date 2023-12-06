0604dbe95dee836eb964b114cf9cab0a808ac99a
ehisgodswill <ehisgodswill@gmail.com>
2023-12-06 18:59:18 +0100

    5

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-  newmatrix = []
-  for row in matrix:
-    newmatrix.append([row[i] * row[i] for i in range(len(row))])
-  return newmatrix
+	return [[row[i]**2 for i in range(len(row))] for row in matrix]
